ba71d9b8fdffa94d87e983674428cb297e24b9a1
huija <1150555483@qq.com>
2022-11-28 17:07:56 +0800

    cupper theme upgrade

--- update_themes.sh
+++ update_themes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # git submodule add git@github.com:huija/cupper-hugo-theme.git themes/cupper-hugo-theme
 # git submodule update --init --recursive
-git submodule update --remote --merge
+git submodule update --remote
 
 # hugo new post/xxx.md
 # hugo new _index.md
